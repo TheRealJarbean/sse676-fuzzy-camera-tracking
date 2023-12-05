0f521bb8df3748713bc957844b4d4c684f4c2c14
therealjarbean <jaron.e.anderson@gmail.com>
2023-12-05 15:54:07 -0500

    feat: servo tester

---
+++ test/servo_tester.py
@@ -0,0 +1,27 @@
+import keyboard
+from gpiozero import Servo
+from time import sleep
+
+while(True):
+    selection = input('Input servo to test. (pan/tilt): ')
+    if selection == 'tilt':
+        servo = Servo(17)
+        break
+    elif selection == 'pan':
+        # TODO: Plug pan servo into a different port
+        break
+    print('Invalid selection.')
+
+# Servo values range from -1 (min) to 1 (max)
+
+while(True):
+    value = input('Input value to move servo (\'exit\' to stop): ')
+    if value == 'exit':
+        print('Exiting...')
+        break
+    value = float(value)
+    if value >= -1 and value <= 1:
+        servo.value = value
+        sleep(1)
+    else:
+        print('Invalid value.')
